0f8df9dceca9e18160b5193df1f334b6121050fc
Danny Luu <hisoka365@gmail.com>
2015-12-10 23:00:00 -0500

    Updated function

--- src/spiralArrayProgram/spiralArrayProgram.java
+++ src/spiralArrayProgram/spiralArrayProgram.java
@@ -1,13 +1,28 @@
 package spiralArrayProgram;
 
+/**
+ * Practice question on creating a spiral array.
+ * @author Hisoka
+ *
+ */
 public class spiralArrayProgram {
 
+	// The array will be constructed as an MxN array.
+	public spiralArrayProgram() {
+		
+	}
+	public static int m = 10;
+	public static int n = 10;	
+	public static int max = m * n;
+	private int [][] array = new int[m][n];
+	
+	int count = 1;
+
+	/**
+	 * Main Function to call when creating the Spiral Array.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int m = 5;
-		int n = 5;
-		
-		
 		spiralArrayProgram sap = new spiralArrayProgram();
 		int [][] array = sap.createArray(m, n);
 		
@@ -21,66 +36,51 @@ public class spiralArrayProgram {
 	 * @return
 	 */
 	private int[][] createArray(int m, int n) {
-		int count, max;
-		int [][] array;
-
-		if (m >= 1 && n >= 1) {
-	
-			count = 1;
-			max = m * n;
-			array = new int[m][n];
-			
-			int minM = 0;
-			int maxM = m-1;
-			
-			int minN = 0;			
-			int maxN = n-1;
-			
-			while (count <= max) {
-				
-				for (int right = minN; right <= maxN; right++) {
-					array[minM][right] = count++;
-					//System.out.println(count++ + " " + minM + "," + right);
-				}
-				minM +=1;
-				for (int down = minM; down <= maxM; down++) {
-					array[down][maxN] = count++;
-					//System.out.println(count++ + " " + down + "," + maxN);
-				}
-				maxN -= 1;
-				for (int left = maxN; left >= minN; left--) {
-					array[maxM][left] = count++;
-					//System.out.println(count++ + " " + maxM + "," + left);
-				}
-				maxM -= 1;
-				for (int up = maxM; up >= minM; up--) {
-					array[up][minN] = count++;
-					//System.out.println(count++ + " " + up + "," + minN);
-				}
-				minN += 1;
-			}
-			
-			
-			
-		} else {
-			array = new int[1][1];
-		}
+		int minM = 0;
+		int maxM = m-1;
+		int minN = 0;			
+		int maxN = n-1;
+		
+		while (count <= max) {	
+			spiralRight(minN, maxN, minM);
+			minM +=1;
+			spiralDown(maxN, minM, maxM);
+			maxN -= 1;
+			spiralLeft(minN, maxN, maxM);
+			maxM -= 1;
+			spiralUp(minN, minM, maxM);
+			minN += 1;
+		}			
 
 		return array;
 	}
 	
-	private void printArray(int[][] a, int m, int n) {
-		for (int i=0; i< m; i++) {
-			System.out.println();
-			for(int j=0; j<n; j++) {
-				if (j == 0) {
-					//System.out.print(i + "," + j + " ");
-				} else {
-					//System.out.print(i + "," + j + " ");
-				}
-			}
+	private void spiralRight(int minN, int maxN, int minM) {
+		for (int right = minN; right <= maxN; right++) {
+			array[minM][right] = count++;
 		}
 		
+	}
+	
+	private void spiralDown(int maxN, int minM, int maxM) {
+		for (int down = minM; down <= maxM; down++) {
+			array[down][maxN] = count++;
+		}
+	}
+	
+	private void spiralLeft(int minN, int maxN, int maxM) {
+		for (int left = maxN; left >= minN; left--) {
+			array[maxM][left] = count++;
+		}
+	}
+	
+	private void spiralUp(int minN, int minM, int maxM) {
+		for (int up = maxM; up >= minM; up--) {
+			array[up][minN] = count++;
+		}
+	}
+	
+	private void printArray(int[][] a, int m, int n) {		
 		for (int i=0; i< m; i++) {
 			System.out.println();
 			for(int j=0; j<n; j++) {
